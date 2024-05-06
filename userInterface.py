faf505524a6f3ebd4fda9b15b6283461379814bd
Nhu Ly <113462967+nhuly3@users.noreply.github.com>
2024-05-05 23:27:20 -0700

    Update userInterface.py

--- userInterface.py
+++ userInterface.py
@@ -1,7 +1,8 @@
 import pygame
+from inventory import Inventory
 
 class UserInterface:
-    def __init__(self):
+    def __init__(self, player):
         self.color_red = (255, 0, 0)
         self.color_green = (0, 255, 0)
         self.color_blue = (0, 0, 255)
@@ -12,12 +13,23 @@ class UserInterface:
         self.regularfont = pygame.font.SysFont("Pixellari", 20)
         self.largefont = pygame.font.SysFont("Pixellari", 40)
 
+        self.inventory=Inventory(player)
+        self.inventoryRender=True
+
         self.text = self.regularfont.render("0", True, self.color_black)
 
     def update(self, fps):
         self.text = self.regularfont.render(str(fps), True, self.color_black)
+        self.inventory.update()
 
     def render(self, display):
         display.blit(self.text, (600, 20))
 
+        if self.inventoryRender==True:
+            self.inventory.render(display)
 
+    def toggleInventory(self):
+        if self.inventoryRender==True:
+            self.inventoryRender=False
+        elif self.inventoryRender==False:
+            self.inventoryRender=True
